032d051874b81a09f8c3f7bc0f57e3e2b70027bc
Edward Timmer <edtimmer@gmail.com>
2024-09-29 12:56:19 -0400

    adjust loader styling

--- src/App.tsx
+++ src/App.tsx
@@ -46,9 +46,9 @@ function App() {
             </Suspense>
           </Canvas>
           <Loader
-            containerStyles={{ backgroundColor: 'rgba(1, 1, 1, 0.8)', width: '80rem', height: '40rem', zIndex: '100' }} // Style of the loader container
-            innerStyles={{ color: 'white' }} // Style of the inner loading text
-            dataStyles={{ color: 'white' }} // Style of the loading percentage text
+            containerStyles={{ backgroundColor: 'rgba(1, 1, 1, 0.8)', width: '100vw', height: '100vh', zIndex: '100' }} // Style of the loader container
+            innerStyles={{ color: 'white', fontSize: '36px' }} // Style of the inner loading text
+            dataStyles={{ color: 'white', fontSize: '36px' }} // Style of the loading percentage text
             dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`} // Customize text
           />
         </LogoContainer>
